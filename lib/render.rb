2e030e6b537617797a3f579056e1346e70f04afc
adam <student@epicodus.com>
2015-05-19 14:17:54 -0700

    add new images for background.

--- lib/render.rb
+++ lib/render.rb
@@ -3,28 +3,26 @@ require './lib/floor'
 
 class GameWindow < Gosu::Window
   def initialize
-    super(400, 400, false) #map size
+    super(1280, 720, false) #map size
     self.caption = "Render Map" #window title
-    @image = Gosu::Image.new(self, "./media/one.png", false) # image tile 1
-    @image_2 = Gosu::Image.new(self, "./media/two.png", false) # image tile 2
-
+    @floor_image = Gosu::Image.new(self, "./media/floor.png", false) # image tile 1
+    @wall_image = Gosu::Image.new(self, "./media/wall.gif", false) # image tile 2
     @floor = Floor.new({:width => 25, :height => 25}) # call toby's mapmaker
-    @floor.create_boundaries 
-    @scaler = 15 #scales the size of the image tiles to account for image size
+    @floor.create_boundaries
+    @scaler = 16 #scales the size of the image tiles to account for image size
   end
 
   def draw
     @floor.map.each_index do |y|
       @floor.map[y].each_index do |x|
         if(@floor.is_solid?(x, y))
-          @image.draw(x*@scaler, y*@scaler, 1)
+          @wall_image.draw(x*@scaler, y*@scaler, 1)
         else
-          @image_2.draw(x*@scaler, y*@scaler, 1)
+          @floor_image.draw(x*@scaler, y*@scaler, 1)
         end
       end
     end
   end
-
 end
 
 window = GameWindow.new
