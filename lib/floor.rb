21063833b9a63bb10e720dcadbc46893e76345ad
adam <student@epicodus.com>
2015-05-19 10:43:59 -0700

    updated Floor class, borders added.

--- lib/floor.rb
+++ lib/floor.rb
@@ -5,9 +5,38 @@ class Floor
   def initialize (attributes)
     @width = attributes[:width]
     @height = attributes[:height]
-    @map = Array.new(@width)
-    @map.each_index() do |index|
-      @map[index] = Array.new(@height, false)
+    @map = Array.new(@height)
+    @map.each_index() do |row_index|
+      @map[row_index] = Array.new(@width, false)
+    end
+  end
+
+  def create_boundaries
+    @map.each_index() do |y|
+      @map[y].each_index() do |x|
+        if(x == 0 || y == 0 || x == @width-1 || y == @height-1)
+          @map[x][y] = true
+        end
+      end
+    end
+  end
+
+  # BELOW FUNCTIONS FOR TESTING ONLY
+
+  def self.make_test_floor
+    return Floor.new({:width => 25, :height => 25})
+  end
+
+  def print_map
+    @map.each_index() do |y|
+      @map[y].each_index() do |x|
+        if(@map[x][y])
+          print('#')
+        else
+          print('.')
+        end
+      end
+      print("\n")
     end
   end
 
--- spec/floor_spec.rb
+++ spec/floor_spec.rb
@@ -5,8 +5,20 @@ describe(Floor) do
 
   describe('#initialize') do
     it("Creates an empty floor.") do
-      test_map = Floor.new({:width => 10, :height => 10})
-      expect(test_map.map[4][4]).to(eq(false))
+      test_floor = Floor.new({:width => 10, :height => 10})
+      expect(test_floor.map[4][4]).to(eq(false))
+    end
+  end
+
+  describe('#create_boundaries') do
+    it("Creates walls around the edges of the map.") do
+      test_floor = Floor.new({:width => 10, :height => 10})
+      test_floor.create_boundaries()
+      expect(test_floor.map[0][0]).to(eq(true))
+      expect(test_floor.map[0][9]).to(eq(true))
+      expect(test_floor.map[9][0]).to(eq(true))
+      expect(test_floor.map[9][9]).to(eq(true))
+      expect(test_floor.map[4][4]).to(eq(false))
     end
   end
 
