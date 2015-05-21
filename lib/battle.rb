ca2e435147f7e5bf3b690305a03e00baa417ac75
Ginkko <Incubusalec@gmail.com>
2015-05-21 12:26:49 -0700

    update monsters with monster image path.

--- lib/battle.rb
+++ lib/battle.rb
@@ -13,43 +13,50 @@ class Battle < ActiveRecord::Base
       level = 0
       vit = 2
       strength = 20
+      image_path = './media/ghoul.png'
     elsif monster_roll == 1
       name = 'Goblin'
       level = 1
       vit = 5
       strength = 10
       weapon_category = 'dagger'
-  elsif monster_roll == 2
+      image_path = './media/goblin.png'
+    elsif monster_roll == 2
       name = 'Kobold'
       level = 0
       vit = 1
       strength = 10
       weapon_category = 'candle'
-  elsif monster_roll == 3
+      image_path = './media/kobold.png'
+    elsif monster_roll == 3
       name = 'Orc'
       level = 3
       vit = 5
       strength = 10
       weapon_category = 'axe'
+      image_path = './media/orc.png'
     elsif monster_roll == 4
       name = 'Nekker'
       level = 1
       vit = 8
       strength = 15
+      image_path = './media/nekker.png'
     elsif monster_roll == 5
       name = 'Mindflayer'
       level = 2
       vit = 2
       strength = 4
       weapon_category = 'wand'
+      image_path = './media/mindflayer.png'
     elsif monster_roll == 6
       name = 'Gorgon'
       level = 3
       vit = 15
       strength = 12
+      image_path = './media/gorgon.png'
     end
 
-    monster = Entity.create(str: strength, vit: vit, name: name, level: level, pc?: false, alive?: true)
+    monster = Entity.create(str: strength, vit: vit, name: name, level: level, pc?: false, alive?: true, image_path: image_path)
     monster.level_up(rand(6))
     max_health = monster.get_max_health
     monster.update(health: max_health)
